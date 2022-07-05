c5ace6118f3ba8552d6cdf4af11f3e092e497810
SamarthAnand <201851108@iiitvadodara.ac.in>
2022-07-05 12:26:34 +0530

    Birthdays today List

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+import GetData from './GetData';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [person, setPerson] = useState(data);
+
+  function clearAll() {
+    setPerson([]);
+  }
+  return (<main>
+    <section className="container">
+      <h3>
+        Birthday list
+      </h3>
+      <h4>{person.length} Bday's Today </h4>
+      <List person={person} />
+      <button style={{ width: '100%' }} onClick={() => { clearAll() }}> Clear All </button>
+    </section>
+  </main>);
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const List = () => {
+const List = (props) => {
   return (
     <>
-      <h2>list component</h2>
+      {props.person.map((person) => {
+        const { id, name, age, image } = person;  /** we can destructure the json data like this .*/
+        return (
+          <div key={person.id} className='person'>
+            <img src={person.image} alt={person.name}></img>
+            <div>
+              <h5 >{person.name}</h5>
+              <h6>{person.age} years</h6>
+              <button onClick={() => { alert(`Happy Birthday ${person.name}`) }}>Wish me</button>
+            </div>
+
+
+          </div>);
+      })
+      }
     </>
   );
 };
